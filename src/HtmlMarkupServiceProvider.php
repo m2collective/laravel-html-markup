5c33565089efaf0d760eb0e131c06ea02ed2cd20
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-24 16:07:11 +0300

    Update HtmlMarkupServiceProvider.php

--- src/HtmlMarkupServiceProvider.php
+++ src/HtmlMarkupServiceProvider.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace M2Collective\HtmlMarkup;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use M2Collective\HtmlMarkup\Commands\ConfigPublishCommand;
+use M2Collective\HtmlMarkup\Commands\ViewPublishCommand;
 
 final class HtmlMarkupServiceProvider extends ServiceProvider
 {
@@ -29,13 +31,28 @@ final class HtmlMarkupServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->loadViewsFrom(
+            __DIR__ . '/../resources/views',
+            'html-markup'
+        );
+
+        Blade::componentNamespace(
+            'M2Collective\\HtmlMarkup\\Views\\Components',
+            'html-markup'
+        );
+
         $this->publishes([
             __DIR__ . '/../config/html-markup.php' => config_path('html-markup.php'),
         ], 'm2collective:html-markup:publish-config');
 
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-html-markup'),
+        ], 'm2collective:html-markup:publish-view');
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 ConfigPublishCommand::class,
+                ViewPublishCommand::class,
             ]);
         }
     }
