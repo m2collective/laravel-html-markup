218dbe8a25372c352ad1a9ac55632100d5e2a016
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-24 16:07:08 +0300

    Create ViewPublishCommand.php

---
+++ src/Commands/ViewPublishCommand.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\HtmlMarkup\Commands;
+
+use Illuminate\Console\Command;
+
+final class ViewPublishCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'm2collective:html-markup:publish-view';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Publishing the view files';
+
+    /**
+     * @return void
+     */
+    public function handle() : void
+    {
+        $this->call('vendor:publish', [
+            '--tag' => 'm2collective:html-markup:publish-view'
+        ]);
+    }
+}
