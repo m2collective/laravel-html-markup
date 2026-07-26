ae2ae3225d91e8338239001e549f66b9c85273ca
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-26 13:48:50 +0300

    Update HtmlMarkupService.php

--- src/HtmlMarkupService.php
+++ src/HtmlMarkupService.php
@@ -3,7 +3,44 @@ declare(strict_types=1);
 
 namespace M2Collective\HtmlMarkup;
 
-final class HtmlMarkupService implements HtmlMarkup
-{
+use M2Collective\HtmlMarkup\Views\Properties\Contracts\Body;
+use M2Collective\HtmlMarkup\Views\Properties\Contracts\Html;
 
+final readonly class HtmlMarkupService implements HtmlMarkup
+{
+    /**
+     * @var Html
+     */
+    public Html $html;
+
+    /**
+     * @var Body
+     */
+    public Body $body;
+
+    /**
+     * @param Html $html
+     * @param Body $body
+     */
+    public function __construct(Html $html, Body $body)
+    {
+        $this->html = $html;
+        $this->body = $body;
+    }
+
+    /**
+     * @return Html
+     */
+    public function getHtml(): Html
+    {
+        return $this->html;
+    }
+
+    /**
+     * @return Body
+     */
+    public function getBody(): Body
+    {
+        return $this->body;
+    }
 }
