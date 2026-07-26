9e935026e05f0d8f4472c271fa903562a7824e44
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-26 13:49:01 +0300

    Create Html.php

---
+++ src/Views/Properties/Html.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\HtmlMarkup\Views\Properties;
+
+use M2Collective\HtmlMarkup\Views\Properties\Contracts\Html as HtmlContract;
+
+final class Html implements HtmlContract
+{
+
+}
