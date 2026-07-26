65de01e050256420e23c1850f51ac98fdb1b1177
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-26 13:53:35 +0300

    Update Body.php

--- src/Views/Properties/Body.php
+++ src/Views/Properties/Body.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace M2Collective\HtmlMarkup\Views\Properties;
 
-use M2Collective\HtmlMarkup\Views\Properties\Contracts\Body as BodyContract;
+use M2Collective\HtmlMarkup\Contracts\Views\Properties\Body as BodyContract;
 
 final class Body implements BodyContract
 {
