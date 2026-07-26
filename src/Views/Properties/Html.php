b57775ce4526ae246fc5e9a80b9e412e46dcbd50
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-26 13:53:24 +0300

    Update Html.php

--- src/Views/Properties/Html.php
+++ src/Views/Properties/Html.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace M2Collective\HtmlMarkup\Views\Properties;
 
-use M2Collective\HtmlMarkup\Views\Properties\Contracts\Html as HtmlContract;
+use M2Collective\HtmlMarkup\Contracts\Views\Properties\Html as HtmlContract;
 
 final class Html implements HtmlContract
 {
