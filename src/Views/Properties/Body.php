5f82bcf0852839ff53517ea555e92355cfd6e4e7
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-26 13:48:53 +0300

    Create Body.php

---
+++ src/Views/Properties/Body.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\HtmlMarkup\Views\Properties;
+
+use M2Collective\HtmlMarkup\Views\Properties\Contracts\Body as BodyContract;
+
+final class Body implements BodyContract
+{
+
+}
