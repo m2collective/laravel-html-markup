b44ecf2fbbd355fcfe781b65e366f0ef92019dc5
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-07-30 14:53:59 +0300

    Update HtmlMarkup.php

--- src/HtmlMarkup.php
+++ src/HtmlMarkup.php
@@ -3,18 +3,7 @@ declare(strict_types=1);
 
 namespace M2Collective\HtmlMarkup;
 
-use M2Collective\HtmlMarkup\Contracts\Views\Properties\Body;
-use M2Collective\HtmlMarkup\Contracts\Views\Properties\Html;
-
 interface HtmlMarkup
 {
-    /**
-     * @return Html
-     */
-    public function getHtml(): Html;
 
-    /**
-     * @return Body
-     */
-    public function getBody(): Body;
 }
